2df8accc5da2cac6329aaa2331ff3fd030d83505
Robert Rainey <r.anthony.rainey@gmail.com>
2024-02-27 20:16:57 -0600

    Added class Backstage

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -46,13 +46,8 @@ class GildedRose
   end
 
   def backstage_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
+    @item = Backstage.new(quality, days_remaining)
+    item.tick
   end
 
   class NormalItem
@@ -95,6 +90,24 @@ class GildedRose
     end
 
     def tick
+    end
+  end
+
+  class Backstage
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
+  
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
     end
   end
 
