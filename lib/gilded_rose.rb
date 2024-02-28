d1e0ac0432271a75bf235659a5889bca1d83eab7
Robert Rainey <r.anthony.rainey@gmail.com>
2024-02-27 19:50:59 -0600

    Added brie_tick and refactored it to match normal_tick

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -68,4 +68,12 @@ class GildedRose
     @quality -= 1
     @quality -= 1 if @days_remaining <= 0
   end
+
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1 
+    @quality += 1 if @days_remaining <= 0 and @quality < 50
+  end
 end
