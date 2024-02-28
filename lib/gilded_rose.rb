c4f3197272a69b58ed6bc5ac5fd092c4524c31f9
Robert Rainey <r.anthony.rainey@gmail.com>
2024-02-27 19:42:21 -0600

    Resolved second and fourth 'Normal Item' case

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -59,7 +59,11 @@ class GildedRose
   end
 
   def normal_tick
-    @quality -= 1
+    if @days_remaining > 0
+      @quality -= 1
+    elsif @days_remaining <= 0
+      @quality -= 2
+    end
     @days_remaining -= 1
   end
 
