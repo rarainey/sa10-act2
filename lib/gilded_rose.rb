a9aa54a96ef56a3b7820b8f4d5bac2190da720e2
Robert Rainey <r.anthony.rainey@gmail.com>
2024-02-27 20:23:58 -0600

    Refactored GildedRose class to create items

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,37 +1,33 @@
 class GildedRose
-  attr_reader :name, :days_remaining, :quality
+  attr_reader :item
 
   def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
+    @item = class_for(name).new(quality, days_remaining)
   end
 
   def quality 
-    return item.quality if item
-    @quality
+    item.quality
   end
 
   def days_remaining
-    return item.days_remaining if item
-    @days_remaining
+    item.days_remaining
+  end
+
+  def class_for(name)
+    case name
+    when 'Normal Item'
+      NormalItem
+    when 'Aged Brie'
+      Brie
+    when 'Sulfuras, Hand of Ragnaros'
+      Sulfuras
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      Backstage
+    end
   end
 
   def tick
-    case name
-    when 'Normal Item'
-      @item = NormalItem.new(quality, days_remaining)
-      item.tick
-    when 'Aged Brie'
-      @item = Brie.new(quality, days_remaining)
-      item.tick
-    when 'Sulfuras, Hand of Ragnaros'
-      @item = Sulfuras.new(quality, days_remaining)
-      item.tick
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      @item = Backstage.new(quality, days_remaining)
-      item.tick
-    end
+    item.tick
   end
 
   class NormalItem
